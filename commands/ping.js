134d188c152e6844b0076503de99d2ab2e2ea5af
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-05-20 21:02:39 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,5 @@
 module.exports.run = async (bot, message, args) => {
-	message.reply(`Pong! ${bot.pings[0]}`)
+	message.reply(`Pong! \`${bot.pings[0]}ms\``)
 }
 module.exports.help = {
 	name: "ping"
